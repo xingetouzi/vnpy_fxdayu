997e2308a55a1bedfb842655f9eae5e5aa1f50b4
ukamoy <rinzia@msn.com>
2018-08-24 16:44:11 +0800

    Minor Improvement

---
+++ example/启动VNPY.bat
@@ -0,0 +1,3 @@
+CD C:\ProgramData\Anaconda3\Lib\site-packages\vnpy_fxdayu-1.0.3-py3.6.egg\vnpy\trader
+python run_vnpy.py
+pause
--- vnpy/__init__.py
+++ vnpy/__init__.py
@@ -1,4 +1,4 @@
 # encoding: UTF-8
 
-__version__ = '1.0.3'
+__version__ = '1.0.4'
 __author__ = 'Xiaoyou Chen'
