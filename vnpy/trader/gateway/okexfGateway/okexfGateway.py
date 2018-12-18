655825f456237643868174a05150d5e3c587a5ae
ukamoy <rinzia@msn.com>
2018-12-18 20:08:27 +0800

    add v3 switch

--- vnpy/trader/gateway/okexfGateway/okexfGateway.py
+++ vnpy/trader/gateway/okexfGateway/okexfGateway.py
@@ -47,16 +47,6 @@ typeMap[(DIRECTION_LONG, OFFSET_CLOSE)] = '4'  # cover
 typeMap[(DIRECTION_SHORT, OFFSET_CLOSE)] = '3' # sell
 typeMapReverse = {v:k for k,v in typeMap.items()}
 
-KlinePeriodMap = {}
-KlinePeriodMap['1min'] = '1min'
-KlinePeriodMap['5min'] = '5min'
-KlinePeriodMap['15min'] = '15min'
-KlinePeriodMap['30min'] = '30min'
-KlinePeriodMap['60min'] = '1hour'
-KlinePeriodMap['1day'] = 'day'
-KlinePeriodMap['1week'] = 'week'
-KlinePeriodMap['4hour'] = '4hour'
-
 ########################################################################
 class OkexfGateway(VtGateway):
     """OKEX期货接口"""
@@ -197,7 +187,42 @@ class OkexfGateway(VtGateway):
         pass
 
     def loadHistoryBar(self,vtSymbol,type_,size=None,since=None,end=None):
-        """V3 temporary down
+        self.loadHistoryBarV1(self,vtSymbol,type_,size=None,since=None,end=None):
+
+    def loadHistoryBarV1(self,vtSymbol,type_,size=None,since=None,end=None):
+        
+        KlinePeriodMap = {}
+        KlinePeriodMap['1min'] = '1min'
+        KlinePeriodMap['5min'] = '5min'
+        KlinePeriodMap['15min'] = '15min'
+        KlinePeriodMap['30min'] = '30min'
+        KlinePeriodMap['60min'] = '1hour'
+        KlinePeriodMap['1day'] = 'day'
+        KlinePeriodMap['1week'] = 'week'
+        KlinePeriodMap['4hour'] = '4hour'
+
+        url = "https://www.okex.com/api/v1/future_kline.do" 
+        type_ = KlinePeriodMap[type_]
+        symbol= vtSymbol.split(VN_SEPARATOR)[0]
+        contractType = symbol[4:]
+        symbol = vtSymbol[:3]+"_usd"
+        params = {"symbol":symbol,
+                    "contract_type":contractType,
+                    "type":type_}
+        if size:
+            params["size"] = size
+        if since:
+            params["since"] = since
+
+        r = requests.get(url, headers={"contentType": "application/x-www-form-urlencoded"}, params = params,timeout=10)
+        text = eval(r.text)
+
+        volume_symbol = vtSymbol[:3]
+        df = pd.DataFrame(text, columns=["datetime", "open", "high", "low", "close", "volume","%s_volume"%volume_symbol])
+        df["datetime"] = df["datetime"].map(lambda x: datetime.fromtimestamp(x / 1000))
+        return df
+
+    def loadHistoryBarV3(self,vtSymbol,type_,size=None,since=None,end=None):
         for key,value in contractMap.items():
             if value == vtSymbol.split(VN_SEPARATOR)[0]:
                 instrument_id = key
@@ -231,20 +256,6 @@ class OkexfGateway(VtGateway):
             params['end'] = end
         else:
             params['end'] = datetime.utcfromtimestamp(datetime.timestamp(datetime.now())).isoformat().split('.')[0]+'Z'
-        V3 down"""
-
-        url = "https://www.okex.com/api/v1/future_kline.do" 
-        type_ = KlinePeriodMap[type_]
-        symbol= vtSymbol.split(VN_SEPARATOR)[0]
-        contractType = symbol[4:]
-        symbol = vtSymbol[:3]+"_usd"
-        params = {"symbol":symbol,
-                    "contract_type":contractType,
-                    "type":type_}
-        if size:
-            params["size"] = size
-        if since:
-            params["since"] = since
 
         r = requests.get(url, headers={"contentType": "application/x-www-form-urlencoded"}, params = params,timeout=10)
         text = eval(r.text)
@@ -255,11 +266,9 @@ class OkexfGateway(VtGateway):
         # df["datetime"] = df["datetime"].map(lambda x: x.strftime("%Y-%m-%d %H:%M:%S"))
         # delta = timedelta(hours=8)
         # df["datetime"] = df["datetime"].map(lambda x: datetime.strptime(x,"%Y-%m-%d %H:%M:%S")-delta)# Alter TimeZone 
-        # df.sort_values(by=['datetime'],axis = 0,ascending =True,inplace = True)
+        df.sort_values(by=['datetime'],axis = 0,ascending =True,inplace = True)
 
         return df#.to_csv('a.csv')
-        
-
 
 ########################################################################
 class OkexfRestApi(RestClient):
