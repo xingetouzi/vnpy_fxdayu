d1449ab398ecdeaa3844f04d8e6a4f0352641d51
caimeng <862786917@qq.com>
2019-03-26 14:30:55 +0800

    update version

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,8 @@ FROM continuumio/anaconda3:5.0.0
 
 ADD ./sources.list /etc/apt/sources.list
 RUN apt-get update 
-RUN apt-get install -y tar
+RUN apt-get install -y tar gcc g++ make
 
 ADD . /vnpy_fxdayu
 WORKDIR /vnpy_fxdayu
-RUN conda install -y python-snappy
-RUN echo ${INSTALL_CTP:-N} | bash install.sh
+RUN echo ${INSTALL_CTP:-N} | bash install-docker.sh
---
+++ install-docker.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+function check_result() {
+    if [ $? -ne 0 ]; then
+        echo " "
+        echo "do command failed for $1 !!!"
+        echo " "
+        exit 1
+    fi
+}
+
+#Build ctp/lts/ib api
+echo "是否要安装'CTP'接口? (Do you need 'CTP' interface?)"
+read -p "Enter [y]n: " var1
+var1=${var1:-y}
+if [ "$var1" = "y" ]; then
+	pushd vnpy/api/ctp
+	bash build.sh
+	popd
+fi
+
+#Install Ta-Lib
+python -c "import talib"
+if [ $? -nq 0 ]; then
+    wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
+    tar -xzf ta-lib-0.4.0-src.tar.gz
+    pushd ta-lib/
+    ./configure --prefix=/usr
+    make
+    make install
+    pip install ta-lib
+    popd
+    rm ta-lib-0.4.0-src.tar.gz
+    rm -rf ta-lib
+fi
+
+conda install -y python-snappy
+
+#Install Python Modules
+pip install -r requirements.txt
+
+#Install vn.py
+python setup.py install
