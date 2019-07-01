32f9a79bfd4b5fe0c1f1b90bf6772d43bd394952
ukamoy <rinzia@msn.com>
2019-07-01 10:38:45 +0800

    new wheels for dependecy

--- install.bat
+++ install.bat
@@ -1,8 +1,7 @@
 ::Install Python Modules
 
-call pip install msgpack-0.5.6-cp36-cp36m-win_amd64.whl
-call pip install TA_Lib-0.4.17-cp36-cp36m-win_amd64.whl
-call pip install python_snappy-0.5.2-cp36-cp36m-win_amd64.whl
+call pip install msgpack-0.6.1-cp37-cp37m-win_amd64.whl
+call pip install TA_Lib-0.4.17-cp37-cp37m-win_amd64.whl
 pip install -r requirements.txt -i https://pypi.douban.com/simple/
 python setup.py install
 pause
